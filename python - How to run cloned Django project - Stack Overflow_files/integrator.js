a612889b59a27e9a073a3794805f82b0d41b9a72
R V Shiva Subramanian <sanders.yorkshire.150b@gmail.com>
2020-07-02 14:07:54 +0530

    add requirements.txt file

---
+++ python - How to run cloned Django project - Stack Overflow_files/integrator.js
@@ -0,0 +1,1 @@
+processGoogleToken({"newToken":"ChAI8Ojw9wUQ6d-pp_PdgJ5CEjkAEmrnIG4LyjCRQI7z0LFnwoYpvRfCJUU1hYOZesj5cSNdK7TrIYhcWEulOJF_aII8MDib1-bQUcs","validLifetimeSecs":300,"freshLifetimeSecs":300,"1p_jar":"","pucrd":""});
